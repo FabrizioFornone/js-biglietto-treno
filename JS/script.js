eaa0da1ec21cac255ffd9fbba9ed13d5950eb1de
FabrizioFornone <fabrizio.fornone@hotmail.it>
2021-12-03 17:26:28 +0100

    Push x Stefano

--- JS/script.js
+++ JS/script.js
@@ -4,8 +4,7 @@ const outputHtml = document.getElementById("output");
 
 // 1° prompt: numero di km da percorrere?
 
-let numKm = prompt('Quanti chilomentri deve percorrere?');
-
+let numKm = prompt("Quanti chilomentri deve percorrere?");
 
 // Tipizzazione della variabile numKm
 
@@ -13,10 +12,9 @@ numKm = parseFloat(numKm);
 
 console.log(numKm);
 
-
 // 2° prompt: età del passeggero?
 
-let age = prompt('Quanti anni ha il passeggero?');
+let age = prompt("Quanti anni ha il passeggero?");
 
 // Tipizzazione della variavbile age
 
@@ -25,20 +23,25 @@ age = parseInt(age);
 console.log(age);
 
 
-//  Definisco la variabile price
+// Se non sono numeri insulta l'utente
 
-let price = numKm * 0.21;
+if (isNaN(numKm) || isNaN(age)) {
+  outputHtml.innerHTML = "Sei scemo? Inserisci un numero";
+} else {
+  let price = price - (price * 0.21);
+}
 
 console.log(price);
+
 
 /* Se l'età del passeggero è < di 18 anni
  price è scontato del 20%,
 altrimenti se l'età è > di 65 anni price è scontato del 40% */
 
 if (age < 18) {
-    price = price - (price * 0.2);
+  price = price - price * 0.2;
 } else if (age > 65) {
-    price = price - (price * 0.4);
+  price = price - price * 0.4;
 }
 
 console.log(price);
@@ -50,4 +53,3 @@ price = Math.round(price * 100) / 100;
 console.log(price);
 
 outputHtml.innerHTML = `Il costo complessivo del biglietto è ${price} €`;
-
