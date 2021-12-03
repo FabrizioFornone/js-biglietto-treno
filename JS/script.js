588636d5ce10d203aeeb935007acf2cfaebc1ab2
FabrizioFornone <fabrizio.fornone@hotmail.it>
2021-12-03 15:33:15 +0100

    conversione di numKm e age in variabili, creazione variabile price

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,21 @@
+// Dichiarazione della costante outputHtml
+
+const outputHtml = document.getElementById("output");
+
 // 1° prompt: numero di km da percorrere?
 
+let numKm = prompt('Quanti chilomentri deve percorrere?');
+
+numKm = parseInt(numKm);
+
 // 2° prompt: età del passeggero?
+
+let age = prompt('Quanti anni ha il passeggero?');
+
+age = parseInt(age);
+
+//  Definisco la variabile price
+
+let price = numKm * 0.21;
+
+
